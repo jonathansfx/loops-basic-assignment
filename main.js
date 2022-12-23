cdd3aea9a80a2a80dcb67837de06a0c265cbbdd0
jonathansfx <jonathanluo22@gmail.com>
2022-12-23 11:46:21 -0700

    fnish

---
+++ main.js
@@ -0,0 +1,28 @@
+// 1.
+// for (n = 1; n <= 500; n++) {
+//   console.log("I'm so happy");
+// }
+
+// 2.
+// for (n = 12; n <= 800; n += 4) {
+//   console.log(n);
+// }
+
+// 3.
+// for (n = 55; n >= 11; n -= 2) {
+//   console.log(n);
+// }
+
+// 4.
+// let total = 0;
+// for (term = 5; term <= 50; term ++) {
+//   total += term;
+//   console.log(total);
+// }
+
+// 5.
+// let total = 0;
+// for (term = 10; term <= 100; term += 10){
+// total += term;
+// console.log("total");
+// }
